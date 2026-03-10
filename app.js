0e4ce1e0fad82435f9fe056f71c021ab15e8e2e2
MDynasty <MingDynastyAI@protonmail.com>
2026-03-10 18:45:58 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ SUPABASE CONNECTION
 ----------------------- */
 
 const supabaseUrl = "https://rryuicpnjxxzsmkotgrj.supabase.co"
-const supabaseKey = "YOUR_PUBLIC_ANON_KEY"
 
-const supabaseClient = window.supabase.createClient(
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyeXVpY3Buanh4enNta290Z3JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNTY1NzYsImV4cCI6MjA4ODYzMjU3Nn0.283wfb_yVscOYWHigTbIFjm6GIeVmSiVuM-XwyinNBc"
+
+const supabaseClient = supabase.createClient(
   supabaseUrl,
   supabaseKey
 )
 
+console.log("Supabase connected")
 /* -----------------------
 INGREDIENT PARSER
 ----------------------- */
